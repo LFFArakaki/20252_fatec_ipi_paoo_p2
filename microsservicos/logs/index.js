c50a3dd58f890c9ef91925579d7ba27a2ae26a97
Luis Fellipe Franco Arakaki <luisfellipe.arakaki@gmail.com>
2025-11-19 18:24:03 -0300

    feat(p2): implementa servico de log de eventos

---
+++ microsservicos/logs/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const axios = require('axios')
+const { v4: uuidv4 } = require('uuid')
+const app = express()
+app.use(express.json())
+
+const baseLogs = []
+
+app.post('/eventos', async (req, res) => {
+  const evento = req.body
+  console.log(evento)
+  const idLog = uuidv4()
+  const data = Date()
+  const log = {id: idLog, dataHora: data, tipoEvento: evento.type, dados: evento.payload}
+  baseLogs.push(log)
+  console.log(log)
+  res.end()
+})
+
+app.get('/logs', (req, res) => {
+  res.json(baseLogs)
+})
+
+const port = 8000
+app.listen(port, () => {
+  console.log(`Logs. Porta ${port}.`)
+})
